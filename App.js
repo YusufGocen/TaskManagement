6eac079618e1620cf08b86c82dbb61710b1cec4c
Yusuf Göçen <128933870+YusufGocen@users.noreply.github.com>
2025-11-12 17:33:48 +0300

    Fıx: removed addTask

--- App.js
+++ App.js
@@ -3,7 +3,6 @@ import { StyleSheet, Text, View } from 'react-native';
 import Home from './Screens/Home/Home'
 import SplashScreen from './Screens/SplashScreen/SplashScreen';
 import Detail from './components/TaskBoard/TaskDetail/Detail'
-import AddTask from './Screens/AddTask/AddTask'
 import { useEffect,useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
